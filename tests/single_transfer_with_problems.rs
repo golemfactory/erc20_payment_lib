7314332638561bf5a68368c67aaea06c1ee5580e
scx1332 <sieciech.czajka@golem.network>
2023-07-31 20:28:58 +0200

    fix db name

--- tests/single_transfer_with_problems.rs
+++ tests/single_transfer_with_problems.rs
@@ -30,13 +30,14 @@ pub struct EndpointSimulateProblems {
 }
 
 
-#[test_case(0.0; "low error probability")]
-#[test_case(0.0; "medium error probability")]
-#[test_case(0.0; "high error probability")]
+#[test_case(0.5; "low error probability")]
+#[test_case(0.6; "medium error probability")]
+#[test_case(0.7; "high error probability")]
 #[tokio::test(flavor = "multi_thread")]
 async fn test_gas_transfer(error_probability: f64) -> Result<(), anyhow::Error> {
     let geth_container = exclusive_geth_init(Duration::from_secs(600)).await;
-    let conn = create_sqlite_connection(None, Some("test_gas_transfer.sqlite"), true).await?;
+    let rand_db_name = format!("test_gas_transfer_{}.sqlite", rand::random::<u64>());
+    let conn = create_sqlite_connection(None, Some(&rand_db_name), true).await?;
 
     let mut config = config::Config::load("config-payments-local.toml").await?;
     let proxy_key = "erc20_transfer";
@@ -143,88 +144,6 @@ async fn test_gas_transfer(error_probability: f64) -> Result<(), anyhow::Error> 
     assert_eq!(
         res["0x41162e565ebbf1a52ec904c7365e239c40d82568"].token_decimal,
         Some("0".to_string())
-    );
-
-    //it's good idea to close sqlite connection before exit, thus we are sure that all transactions were written to db
-    //TODO: wrap into RAII async drop hack
-    conn.close().await;
-    Ok(())
-}
-
-#[tokio::test(flavor = "multi_thread")]
-async fn test_erc20_transfer() -> Result<(), anyhow::Error> {
-    let geth_container = exclusive_geth_init(Duration::from_secs(600)).await;
-    let conn = create_sqlite_connection(None, Some("test_erc20_transfer.sqlite"), true).await?;
-
-    let mut config = config::Config::load("config-payments-local.toml").await?;
-    config.chain.get_mut("dev").unwrap().rpc_endpoints = vec![format!(
-        "http://127.0.0.1:{}/web3/erc20_transfer",
-        geth_container.web3_proxy_port
-    )];
-
-    let chain_cfg = config
-        .chain
-        .get("dev")
-        .ok_or(err_custom_create!("Chain dev not found in config file",))?;
-
-    //account 0x653b48E1348F480149047AA3a58536eb0dbBB2E2
-    let private_keys =
-        load_private_keys("0228396638e32d52db01056c00e19bc7bd9bb489e2970a3a7a314d67e55ee963")?;
-
-    insert_token_transfer(
-        &conn,
-        &create_token_transfer(
-            Address::from_str("0xbfb29b133aA51c4b45b49468F9a22958EAFeA6fa").unwrap(),
-            Address::from_str("0xf2f86a61b769c91fc78f15059a5bd2c189b84be2").unwrap(),
-            chain_cfg.chain_id,
-            Some("test_payment"),
-            Some(chain_cfg.token.clone().unwrap().address),
-            U256::from(2222000000000000222_u128),
-        ),
-    )
-    .await?;
-
-    let sp = start_payment_engine(
-        &private_keys.0,
-        "",
-        config.clone(),
-        Some(conn.clone()),
-        Some(AdditionalOptions {
-            keep_running: false,
-            generate_tx_only: false,
-            skip_multi_contract_check: false,
-        }),
-    )
-    .await?;
-    sp.runtime_handle.await?;
-
-    let account_balance_options = AccountBalanceOptions {
-        chain_name: "dev".to_string(),
-        accounts:
-            "0x653b48E1348F480149047AA3a58536eb0dbBB2E2,0xf2f86a61b769c91fc78f15059a5bd2c189b84be2"
-                .to_string(),
-        show_gas: true,
-        show_token: true,
-        block_number: None,
-        tasks: 4,
-        interval: Some(0.001),
-    };
-
-
-
-    let res = account_balance(account_balance_options.clone(), &config).await?;
-
-    assert_eq!(
-        res["0xf2f86a61b769c91fc78f15059a5bd2c189b84be2"].gas,
-        Some("0".to_string())
-    );
-    assert_eq!(
-        res["0xf2f86a61b769c91fc78f15059a5bd2c189b84be2"].token_decimal,
-        Some("2.222000000000000222".to_string())
-    );
-    assert_eq!(
-        res["0xf2f86a61b769c91fc78f15059a5bd2c189b84be2"].token_human,
-        Some("2.22 tGLM".to_string())
     );
 
     //it's good idea to close sqlite connection before exit, thus we are sure that all transactions were written to db
