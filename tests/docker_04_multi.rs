d6351d227e6c09ecfdcb8d220021e9d2944d813e
scx1332 <sieciech.czajka@golem.network>
2023-08-26 00:18:59 +0200

    fmt

--- tests/docker_04_multi.rs
+++ tests/docker_04_multi.rs
@@ -4,7 +4,7 @@ use erc20_payment_lib::misc::load_private_keys;
 use erc20_payment_lib::runtime::DriverEventContent::*;
 use erc20_payment_lib::runtime::{start_payment_engine, DriverEvent};
 use erc20_payment_lib::transaction::create_token_transfer;
-use erc20_payment_lib::utils::{u256_to_rust_dec};
+use erc20_payment_lib::utils::u256_to_rust_dec;
 use erc20_payment_lib_test::*;
 use std::str::FromStr;
 use std::time::Duration;
