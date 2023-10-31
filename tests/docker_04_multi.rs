101e6bf331db3fd6b2b55813abc9ccccbc4960b7
Kamil Koczurek <koczurekk@gmail.com>
2023-10-31 14:42:07 +0100

    Make VerifyTransactionResult carry total tx amount

--- tests/docker_04_multi.rs
+++ tests/docker_04_multi.rs
@@ -194,10 +194,10 @@ async fn test_multi_erc20_transfer(payment_count: usize, use_direct_method: bool
             let to_str = Address::from_str(addr).unwrap();
             let fr_str_wrong = Address::from_str("0xcfb29b133aa51c4b45b49468f9a22958eafea6fa").unwrap();
             let to_str_wrong = Address::from_str("0x02f86a61b769c91fc78f15059a5bd2c189b84be2").unwrap();
-            assert_eq!(verify_transaction(&web3, 987789, tx_hash,fr_str,to_str,U256::from(*val), token_address).await.unwrap().verified, true);
-            assert_eq!(verify_transaction(&web3, 987789, tx_hash,fr_str,to_str_wrong,U256::from(*val), token_address).await.unwrap().verified, false);
-            assert_eq!(verify_transaction(&web3, 987789, tx_hash,fr_str_wrong,to_str,U256::from(*val), token_address).await.unwrap().verified, false);
-            assert_eq!(verify_transaction(&web3, 987789, tx_hash,fr_str,to_str,U256::from(*val + 1), token_address).await.unwrap().verified, false);
+            assert_eq!(verify_transaction(&web3, 987789, tx_hash,fr_str,to_str,U256::from(*val), token_address).await.unwrap().verified(), true);
+            assert_eq!(verify_transaction(&web3, 987789, tx_hash,fr_str,to_str_wrong,U256::from(*val), token_address).await.unwrap().verified(), false);
+            assert_eq!(verify_transaction(&web3, 987789, tx_hash,fr_str_wrong,to_str,U256::from(*val), token_address).await.unwrap().verified(), false);
+            assert_eq!(verify_transaction(&web3, 987789, tx_hash,fr_str,to_str,U256::from(*val + 1), token_address).await.unwrap().verified(), false);
         }
     }
 
