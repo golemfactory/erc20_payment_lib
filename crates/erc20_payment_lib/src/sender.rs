3f68929dcc83eaa0aae5e4a4140f9b5d2f799afa
scx1332 <sieciech.czajka@golem.network>
2024-02-07 10:47:46 +0100

    move from mod to named modules

---
+++ crates/erc20_payment_lib/src/sender.rs
@@ -0,0 +1,7 @@
+mod allowance;
+mod batching;
+pub mod process;
+mod service;
+
+pub use allowance::*;
+pub use service::*;
--- crates/erc20_payment_lib/src/sender/mod.rs
+++
@@ -1,7 +0,0 @@
-mod allowance;
-mod batching;
-pub mod process;
-mod service;
-
-pub use allowance::*;
-pub use service::*;
