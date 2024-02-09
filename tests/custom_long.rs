2e144ec965d0a3eab0df750df8f6b1dd7972e3fa
scx1332 <sieciech.czajka@golem.network>
2024-02-09 19:13:58 +0100

    Multi account support

--- tests/custom_long.rs
+++ tests/custom_long.rs
@@ -11,9 +11,13 @@ async fn test_durability_custom() -> Result<(), anyhow::Error> {
     let transfers_at_once = transfers_at_once
         .parse::<usize>()
         .expect("ERC20_TEST_MAX_IN_TX has to be number");
+    let accounts_count = env::var("ERC20_TEST_ACCOUNTS_COUNT").unwrap_or("1".to_string());
+    let accounts_count = accounts_count
+        .parse::<usize>()
+        .expect("ERC20_TEST_ACCOUNTS_COUNT has to be number");
     let transfer_interval = env::var("ERC20_TEST_TRANSFER_INTERVAL").unwrap_or("0.01".to_string());
     let transfer_interval = transfer_interval
         .parse::<f64>()
         .expect("ERC20_TEST_TRANSFER_INTERVAL has to be number");
-    test_durability(transfer_count, transfer_interval, transfers_at_once).await
+    test_durability(accounts_count, transfer_count, transfer_interval, transfers_at_once).await
 }
